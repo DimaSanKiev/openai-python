82a5e53a52af4017eb4be9a0228e5d102599353f
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2023-10-28 02:32:09 +0200

    Natural language to SQL converter project

---
+++ 02-nlp_to_sql/db_utils.py
@@ -0,0 +1,37 @@
+from sqlalchemy import create_engine
+from sqlalchemy import text
+
+
+def dataframe_to_database(df, table_name):
+    """
+    Convert a pandas dataframe to a database.
+    :param df: pd.DataFrame which is to be converted to a database
+    :param table_name: Name of the table within the database
+    :return: engine: SQLAlchemy engine object
+    """
+    engine = create_engine(f'sqlite:///:memory:', echo=False)
+    df.to_sql(name=table_name, con=engine, index=False)
+    return engine
+
+
+def handle_response(response):
+    """Handles the response from OpenAI.
+    :param response: Response json from OpenAI
+    :return: Proposed SQL query
+    """
+    query = response["choices"][0]["text"]
+    if query.startswith(" "):
+        query = "SELECT" + query
+    return query
+
+
+def execute_query(engine, query):
+    """
+    Execute a query on a database.
+    :param engine: database engine (SQLAlchemy engine object)
+    :param query: SQL query (string)
+    :return: list of tuples containing the result of the query
+    """
+    with engine.connect() as conn:
+        result = conn.execute(text(query))
+        return result.fetchall()
---
+++ 02-nlp_to_sql/openai_utils.py
@@ -0,0 +1,57 @@
+import openai
+
+
+def create_table_definition_prompt(df, table_name):
+    """
+    This function creates a prompt for the OpenAI API to generate SQL queries.
+    :param df: pd.DataFrame object to automatically extract the table columns
+    :param table_name: Name of the table within the database
+    :return: string containing the prompt for OpenAI
+    """
+
+    prompt = '''### sqlite table, with its properties:
+#
+# {}({})
+#
+'''.format(table_name, ",".join(str(x) for x in df.columns))
+
+    return prompt
+
+
+def user_query_input():
+    """
+    Ask the user what they want to know about the data.
+    :return: user input, string
+    """
+    user_input = input("Tell OpenAI what you want to know about the data: ")
+    return user_input
+
+
+def combine_prompts(fixed_sql_prompt, user_query):
+    """
+    Combine the fixed SQL prompt with the user query.
+    :param fixed_sql_prompt: Fixed SQL prompt, string
+    :param user_query: User query, string
+    :return: Combined prompt
+    """
+    final_user_input = f"### A query to answer: {user_query}\nSELECT"
+    return fixed_sql_prompt + final_user_input
+
+
+def send_to_openai(prompt):
+    """
+    Send the prompt to OpenAI.
+    :param prompt: Prompt to send to OpenAI, string
+    :return: Response from OpenAI
+    """
+    response = openai.Completion.create(
+        engine="gpt-3.5-turbo-instruct",
+        prompt=prompt,
+        temperature=0,
+        max_tokens=150,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+        stop=["#", ";"]
+    )
+    return response
